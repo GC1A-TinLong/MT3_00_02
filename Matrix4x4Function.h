82a909f812502548dd17d7e34673a32724647ffe
GC1A-TinLong <147121460+GC1A-TinLong@users.noreply.github.com>
2024-04-16 15:52:33 +0900

    プロジェクト ファイルを追加します。

---
+++ Matrix4x4Function.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <Novice.h>
+
+struct Matrix4x4 {
+	float m[4][4];
+};
+
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label);
+
+Matrix4x4 Add(const Matrix4x4& m1, const Matrix4x4& m2);
+Matrix4x4 Subtract(const Matrix4x4& m1, const Matrix4x4& m2);
+Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
+Matrix4x4 Inverse(const Matrix4x4& m);
+Matrix4x4 Transpose(const Matrix4x4& m);
+Matrix4x4 MakeIdentity4x4();
